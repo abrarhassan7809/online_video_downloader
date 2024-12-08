b33cd5752c273a08109bf090e842257a23b968ef
Ubuntu <abrarhassan7809@gmail.com>
2024-12-08 23:48:09 +0500

    add and update video and audio functionality

--- downloader.py
+++ downloader.py
@@ -1,14 +1,59 @@
+#downloader.py
+import uuid
+import instaloader
 import yt_dlp
 from fastapi import HTTPException
 import os
+import re
+import requests
 
+session_id = "192799e78e3-7b14fe"
 
-async def download_video(url: str, output_path: str = "downloads"):
+def sanitize_filename(filename):
+    return re.sub(r'[\\/:"*?<>|]+', "_", filename)
+
+async def youtube_download_audio(url: str, quality: str, output_path: str = "downloads"):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    if quality not in ["128", "320"]:
+        raise HTTPException(status_code=400, detail="Invalid quality value.")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': quality
+        }],
+        'postprocessor_args': ['-af', 'aresample=48000'],
+        'noplaylist': True,
+        'quiet': True
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            sanitized_title = sanitize_filename(info['title'])
+            file_path = os.path.join(output_path, f"{sanitized_title}.mp3")
+
+            if not os.path.exists(file_path):
+                raise RuntimeError(f"Audio file {file_path} does not exist.")
+
+            return file_path
+    except yt_dlp.utils.DownloadError as e:
+        raise HTTPException(status_code=400, detail=f"Download error: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+
+async def youtube_download_video(url: str, quality: str, output_path: str = "downloads"):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        'format': f'bestvideo[height<={int(quality)}]+bestaudio/best[height<={int(quality)}]',
+        # 'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'outtmpl': f'{output_path}/%(title)s.mp4',
         'noplaylist': True,
         'quiet': True,
@@ -27,3 +72,16 @@ async def download_video(url: str, output_path: str = "downloads"):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+
+
+def instagram_download_video(url: str, output_path: str = "downloads"):
+    loader = instaloader.Instaloader()
+    post = instaloader.Post.from_shortcode(loader.context, url.split("/")[-2])
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    loader.download_post(post, output_path)
+    for file in os.listdir(output_path):
+        if file.endswith(".mp4"):
+            return os.path.join(output_path, file)
+    return "No media found"
--- main.py
+++ main.py
@@ -1,34 +1,59 @@
+#main.py
 from fastapi import FastAPI, Form, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.responses import JSONResponse
 from chatbot.chat import get_response
-from downloader import download_video
+from downloader import youtube_download_video, instagram_download_video, youtube_download_audio
 import uvicorn
 import os
+import re
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+YOUTUBE_REGEX = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/.+')
+INSTAGRAM_REGEX = re.compile(r'(https?://)?(www\.)?instagram\.com/.+')
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/atoms/download/")
-async def download_video_api(url: str = Form(...)):
+@app.post("/atoms/download/{type}")
+async def download(type: str, url: str = Form(...), format_id: str = Form(...)):
     url = url.strip()
+    print(type, url, format_id)
     if not url.startswith(("http://", "https://")):
         return JSONResponse(content={"error": "Invalid URL"}, status_code=400)
 
-    try:
-        file_path = download_video(url)
-        if 'is not a valid URL' in file_path:
-            return JSONResponse(content={"error": "Invalid URL"}, status_code=400)
-        return FileResponse(file_path, media_type='application/octet-stream', filename=os.path.basename(file_path))
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+    if type == "video":
+        if YOUTUBE_REGEX.match(url):
+            try:
+                file_path = await youtube_download_video(url, format_id)
+                return FileResponse(file_path, media_type='application/octet-stream', filename=os.path.basename(file_path))
+            except Exception as e:
+                return JSONResponse(content={"error": str(e)}, status_code=500)
+
+    elif type == "audio":
+        if YOUTUBE_REGEX.match(url):
+            try:
+                file_path = await youtube_download_audio(url, format_id)
+                return FileResponse(file_path, media_type='application/octet-stream', filename=os.path.basename(file_path))
+            except Exception as e:
+                return JSONResponse(content={"error": str(e)}, status_code=500)
+
+    elif INSTAGRAM_REGEX.match(url) and type == "video":
+        try:
+            file_path = instagram_download_video(url)
+            if 'No media found' in file_path:
+                return JSONResponse(content={"error": "No media found"}, status_code=400)
+            return FileResponse(file_path, media_type='application/octet-stream', filename=os.path.basename(file_path))
+        except Exception as e:
+            return JSONResponse(content={"error": str(e)}, status_code=500)
+
+    return JSONResponse(content={"error": "Unsupported URL or type"}, status_code=400)
 
 @app.post("/predict/")
 async def predict(request: Request):
